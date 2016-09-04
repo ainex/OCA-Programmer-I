eac86e4e0354ba01b877cd21411956c67c7df582
Xenia Ulyanova <ulyanova.xenia@gmail.com>
2016-09-04 17:08:58 +0700

    operators and statements

---
+++ src/operatorsandstatements/AssignmentOperators.java
@@ -0,0 +1,12 @@
+package operatorsandstatements;
+
+/**
+ *
+ */
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        byte x = 10;
+        byte y = 127;
+
+    }
+}
---
+++ src/operatorsandstatements/Loops.java
@@ -0,0 +1,14 @@
+package operatorsandstatements;
+
+/**
+ *
+ */
+public class Loops {
+    public static void main(String[] args) {
+        //endless loop
+        for (int i = 0; i< 10;  ) {
+            i=i++;
+            System.out.println("hello");
+        }
+    }
+}
